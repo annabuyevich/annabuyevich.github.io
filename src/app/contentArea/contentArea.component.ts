10efc35aa4b59da4d516fe0459bdb0304abca55f
annabuyevich <avbuyevich@gmail.com>
2017-08-10 21:40:13 -0500

    Auto-generated commit

--- src/app/contentArea/contentArea.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: `./contentArea.component.html`,
-  styleUrls: ['./contentArea.component.css']
-})
-export class contentAreaComponent {}
--- src/app/projects/projects.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: `./projects.component.html`,
-  styleUrls: ['./projects.component.css']
-})
-export class projectsComponent {}
